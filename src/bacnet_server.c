2befb361a3a50f0add989cb1b6173c94481619fc
Your Name <you@example.com>
2016-05-31 15:19:21 +1000

    got linked list working

--- src/bacnet_server.c
+++ src/bacnet_server.c
@@ -43,8 +43,7 @@ static uint16_t test_data[] = {
 /* start adding linked listed and thread*/
 
 struct list_object_s {
-    char *string;                   /* 8 bytes */
-    int strlen;                     /* 4 bytes */
+    uint16_t modbus_data;                   /* 8 bytes */
     struct list_object_s *next;     /* 8 bytes */
 };
 /* list_head is initialised to NULL on application launch as it is located in 
@@ -55,7 +54,7 @@ static pthread_cond_t list_data_ready = PTHREAD_COND_INITIALIZER;
 static pthread_cond_t list_data_flush = PTHREAD_COND_INITIALIZER;
 static struct list_object_s *list_head;
 
-static void add_to_list(char *input) {
+static void add_to_list(uint16_t input) {
     /* Allocate memory */
     struct list_object_s *last_item;
     struct list_object_s *new_item = malloc(sizeof(struct list_object_s));
@@ -65,8 +64,7 @@ static void add_to_list(char *input) {
     }
 
     /* Set up the object */
-    new_item->string = strdup(input);
-    new_item->strlen = strlen(input);
+    new_item->modbus_data = input;
     new_item->next = NULL;
 
     /* list_head is shared between threads, need to lock before access */
@@ -97,108 +95,23 @@ static struct list_object_s *list_get_first(void) {
     return first_item;
 }
 
-static void *print_and_free(void *arg) {
-    struct list_object_s *cur_object;
-
-    printf("thread is starting\n");
-
-    while (1) {
-        /* Wait until some data is available */
-        pthread_mutex_lock(&list_lock);
-
-        while (!list_head)
-            pthread_cond_wait(&list_data_ready, &list_lock);
-
-        cur_object = list_get_first();
-        /* Release lock, all further accesses are not shared */
-        pthread_mutex_unlock(&list_lock);
 
 
-        printf("t2: String is: %s\n", cur_object->string);
-        printf("t2: String length is %i\n", cur_object->strlen);
-        free(cur_object->string);
-        free(cur_object);
 
-        /* Inform list_flush that some work has been completed */
-        pthread_cond_signal(&list_data_flush);
-	
-    }
-	return 0;
-}
-
-static void list_flush(void) {
-    pthread_mutex_lock(&list_lock);
-
-    while (list_head) {
-        pthread_cond_signal(&list_data_ready);
-        pthread_cond_wait(&list_data_flush, &list_lock);
-    }
-
-    pthread_mutex_unlock(&list_lock);
-}
-
-int main(int argc, char **argv) {
-    modbus_t *ctx;
-    int option_index, c, counter, counter_given = 0;
-    char input[256]; /* On the stack */
-    pthread_t print_thread;
-
-    struct option long_options[] = {
-        { "count",      required_argument,  0, 'c' },
-        { "directive",  no_argument,        0, 'd' },
-        { 0 }
-    };
-
-    while (1) {
-        c = getopt_long(argc, argv, "c:d", long_options, &option_index);
-
-        if (c == -1) break;
-
-        switch (c) {
-            case 'c':
-                printf("Got count argument with value %s\n", optarg);
-                counter = atoi(optarg);
-                counter_given = 1;
-                break;
-            case 'd':
-                printf("Got directive argument\n");
-                break;
-        }
-    }
-
-    /* Print out all items of linked list and free them */
-    /* Fork a new thread */
-    pthread_create(&print_thread, NULL, print_and_free, NULL);
-
-    while (scanf("%256s", input) != EOF) {
-        /* Add word to the bottom of a linked list */
-        add_to_list(input);
-        if (counter_given) {
-            counter--;
-            if (!counter) break;
-        }
-    }
-
-    printf("Linked list object is %li bytes long\n",
-                    sizeof(struct list_object_s));
-
-    /* Block here until all objects have been printed */
-   list_flush();
-
-    return 0;
-}
 
 /* end of adding linked list and thread */
 
 static int Update_Analog_Input_Read_Property(
 		BACNET_READ_PROPERTY_DATA *rpdata) {
-
+    struct list_object_s *cur_object;
+	        /* Release lock, all further accesses are not shared */
     static int index;
     int instance_no = bacnet_Analog_Input_Instance_To_Index(
 			rpdata->object_instance);
 
     if (rpdata->object_property != bacnet_PROP_PRESENT_VALUE) goto not_pv;
-
+    if (list_head== NULL) goto not_pv;
+	cur_object = list_get_first();
     printf("AI_Present_Value request for instance %i\n", instance_no);
     /* Update the values to be sent to the BACnet client here.
      * The data should be read from the head of a linked list. You are required
@@ -209,7 +122,7 @@ static int Update_Analog_Input_Read_Property(
      *     Second argument: data to be sent
      *
      * Without reconfiguring libbacnet, a maximum of 4 values may be sent */
-    bacnet_Analog_Input_Present_Value_Set(0, test_data[index++]);
+    bacnet_Analog_Input_Present_Value_Set(0,  cur_object->modbus_data);
     /* bacnet_Analog_Input_Present_Value_Set(1, test_data[index++]); */
     /* bacnet_Analog_Input_Present_Value_Set(2, test_data[index++]); */
     
@@ -353,9 +266,11 @@ while (1)
   /* Read 5 registers from the address 0 */
   	modbus_read_registers(mb, 12, 1, tab_reg);/*ask for num from Kim*/
 	printf("got value %x\n", tab_reg[0]);
+	add_to_list(tab_reg[0]);
 	}  
   modbus_close(mb);
   modbus_free(mb);
+  
   return (dummy);/* this statement make no sense but keep compile running*/
 }
 /* end modbus com */
